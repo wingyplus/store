dec571eb367ba3e9692626d642e6e515f00f48bd
Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
2016-09-22 14:30:18 +0700

    add db migration

---
+++ db/migrations/20160922140818_CreateTodosTable.sql
@@ -0,0 +1,12 @@
+
+-- +goose Up
+-- SQL in section 'Up' is executed when this migration is applied
+CREATE TABLE todo (
+       id INT AUTO_INCREMENT PRIMARY KEY,
+       title VARCHAR(255) NOT NULL,
+       complete TINYINT(1)
+) ENGINE=INNODB;
+
+-- +goose Down
+-- SQL section 'Down' is executed when this migration is rolled back
+DROP TABLE todo;
